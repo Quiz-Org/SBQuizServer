8c94db9e14d23a859c3917de99daae2ca6b6df4f
FegrusB <fergus@beckerleg.co.uk>
2024-05-20 18:59:57 +0100

    Added remote state and an ecr

---
+++ terraform/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  bucket_name = "quiz-server-tf"
+  table_name  = "quiz-server-tf"
+
+  ecr_repo_name = "quiz-ecr-repo"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,24 @@
 terraform {
+  backend "s3" {
+    bucket         = "quiz-server-tf"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "quiz-server-tf"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
+}
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = local.bucket_name
+  table_name  = local.table_name
+}
+module "ecrRepo" {
+  source        = "./modules/ecr"
+  ecr_repo_name = local.ecr_repo_name
 }
